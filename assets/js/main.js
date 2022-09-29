88293e93efb1fc1c16d9ad52a879e83401138051
SofiaVossen <sofiavossen@gmail.com>
2022-09-29 20:46:18 +0200

    first

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+function myFunction(){
+    let showMe=document.getElementById("vorname").value;
+    console.log(showMe);
+    let showMe2=document.getElementById("nachname").value;
+    console.log(showMe2);
+    let showMe3=document.getElementById("land").value;
+    console.log(showMe3);
+}
